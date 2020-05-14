2690fa2a698e103043d03f61e8127b7228167a27
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-05-14 15:04:39 -0700

    recursion solution for steps

--- steps/index.js
+++ steps/index.js
@@ -1,8 +1,10 @@
-//Create visual steps out of scores(#) and parenthesis, bottom level has the same number
-//of scores as levels of scores plus 
-//add a space on the right hand side for each score every time you add a new step
+//Create visual steps out of scores(#) and parenthesis, bottom most level has the same number
+//of scores as there are levels of scores.
+//add a space on the right hand side of the score symbol, for each level every time you add a new step
 //Write a function that accepts a positive number N
-//The function will console log a step shape with N levels: 1 level looks like this '#'
+//The function will console log strings stacked on top of 
+//each other in a step shape with N levels and the bottom most level has N number of #'s.
+//: 1 level looks like this '#'
 //TWO LEVELS
 // '# ' One space to the right
 // '##'
@@ -18,29 +20,57 @@
 // '### '
 // '####' NO  Space to the right.
 
+
+//Understand base cases to make life easier:
+//If n number is equal to j stop everthing & the function is done.
+//If n is equal to string length console log string and call function agian for the next level of strings
+//if stairs string length is less than or equal the number j then add "#"
+//If Stairs string length is greater than j number add a space " "
+
+//Solved with Reursion (compared to the solution at the bottom: j is like row & stairs used like column)
+function steps(n, j = 0, stairs = "") {
+  //Set all base cases to save infinite recursion: 
+  if (n === j) {
+     return;
+  } 
+  //The only recursive part
+  if (n === stairs.length) { 
+     console.log(stairs);
+     return steps(n, j + 1);
+  }
+  if (stairs.length <= j) {
+     stairs += "#";
+  } else {
+      stairs += " ";
+  }  
+  steps(n, j, stairs);
+}
+
+module.exports = steps;
+
+//Write a function that accepts a positive number N
+//The function will console log strings stacked on top of 
+//each other in a step shape with N levels and the bottom most level has N number of #'s.
 //If column number is equal to or less than row number add "#"
 //If Column number is greater than row number add a space " "
 //Console log answer
 
+// function steps(n) {
 
-function steps(n) {
+//  //First iterate over rows n times
+//  for (var row = 0; row < n; row++) {
+//      //set a variable to empty string to build a string
+//      let stair = '';
 
- //First iterate over rows n times
- for (var row = 0; row < n; row++) {
-     //set a variable to empty string to build a string
-     let stair = '';
-
-     //Iterate over column n times
-     for (var column = 0; column < n; column++) {
+//      //Iterate over column n times
+//      for (var column = 0; column < n; column++) {
         
-        if (column <= row) {
-           stair += '#';
-        } else {
-          stair += ' ';
-        }
-     }
-     console.log(stair);
- }
-}
-
-module.exports = steps;
+//         if (column <= row) {
+//            stair += '#';
+//         } else {
+//           stair += ' ';
+//         }
+//      }
+//      console.log(stair);
+//  }
+// }
