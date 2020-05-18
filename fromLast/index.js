854f6dd2c951ef01845dc1196e6df7d23e4e5833
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-05-17 20:44:44 -0700

    insertAt method

--- fromLast/index.js
+++ fromLast/index.js
@@ -72,6 +72,7 @@ class LinkedList {
   }
 
   //Remove the last node of a linked list. Find the last node and get the node before it to point it at null
+  //Two pointers one traversing through and second one following it which will become the "new last node" after removal of the "current last node"
   removeLast() {
     //If no first node this function is done
     if (!this.head) {
@@ -108,6 +109,7 @@ class LinkedList {
   }
 
   //Get node at index or integer number given
+  //While One pointer loops through add a tick to the counter check if the counter matches the given integer. 
   getAt(integer) {
     //Temperary variables 
     //Pointer to traverse through the list looking for the index matching the counter to return the data 
@@ -126,7 +128,8 @@ class LinkedList {
       //Traversing through each node by looking at next
       node = node.next;
     }
-    //If the loop is never entered or if we complete the loop and never find the exact number index return null
+    //If 1) the while loop is never entered or 2) if we complete the loop and 
+    //keep going because we never find a match to the integer /index then return null
     return null;
   }
 
@@ -143,15 +146,39 @@ class LinkedList {
     }
 
     //Grab the node before the chosen node
+    //getAt method also checks to see if the integer is less than one or more than the size of the linked list
     let previous = this.getAt(integer - 1);
 
-    //To handle out of bounds requests: If given number is passed the last node return;
+    //To handle out of bounds requests: If given number is passed the last node then just return;
     if (!previous || !previous.next) {
-       return;
+       return null;
     }
-    //Link the prior node and the node after chosen node to each other
+    //After getting the chosen node, link the prior node and the node after the chosen node to each other
     previous.next = previous.next.next;
   }
+
+  //Make new node, point previous node to the new node and set new node next key value to 
+  //the next node in line or null if no node
+  insertAt(data, index) {
+    
+    //Insert first node if there are no nodes
+    if (!this.head) {
+       //Point head at new node 
+       this.head = new Node(data);
+       return;
+    }
+    //Insert first node if there are other nodes
+    //New node has a next key pointing at the old head node
+    if (index === 0) {
+       this.head = new Node(data, this.head);
+       return;
+    }
+    //Insert a new node anywhere in the middle of the linked list
+    //Insert a new node if the index is past the last node
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }  
 
 }
 module.exports = { Node, LinkedList };
