57a75efb73e05113c2855f7edfc9d80e3fe2a358
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-05-22 18:43:35 -0700

    tree breadth traverse

--- treeBuild/index.js
+++ treeBuild/index.js
@@ -1,4 +1,9 @@
-//Create a node class its constructor should accept an argument 
+//Create a Node class its constructor should accept an argument that gets assigned to the data property and 
+//initialize an empty array to store children. The Node class should have methods Add & Remove
+
+//Create a Tree class. The Tree constructor should initialize a root property to null.
+//Implement traverseBF (Breadth First) & traverseDF (Depth First) on the Tree class. Each method should accept a function that
+//gets called with every element in the tree
 
 class Node {
   constructor(data) {
@@ -12,7 +17,7 @@ class Node {
 // const node = new Node(data);
     //Push into the current nodes children array
 // this.children.push(node);
-    //Condensed to one line:
+    //Two above lines Condensed to one line:
     this.children.push(new Node(data));
   }
 
@@ -25,16 +30,37 @@ class Node {
       return node.data !== data;
     });
   }
+
 }
 
 class Tree{
-  constructor(root) {
-    root = this.root
+  constructor() {
     this.root = null;
   }
-
-  traverseBFS() {
-
+  //Breadth first function takes in the arguement
+  //TRAVERSEBF  
+  //Do math or Add to each node in a tree
+    traverseBF(fn) {
+      //Create an array to use as a container where we add 10 to each child
+      //Start with the head put head node in array 
+      const nodeArray = [this.root];
+      //while theres an element in the array take out the first element
+      while (nodeArray.length) {
+        //Remove the first element from the array and put it into a variable
+        const node = nodeArray.shift();
+        //Take all the children of the shifted node and put into array. Take all the children out of children array and add them to this array
+        nodeArray.push(...node.children);
+        //Same as spread operator but with a for loop instead
+        // for (let childrens of node.children) {
+        //   nodeArray.push(node.children);
+        // }
+        
+        //Take the node we are working on and pass it to out iterator function
+        //Call function on the individual node we are looking
+        fn(node);
+        //Then throw away each node after processing
+    
+      }
   }
 }
 
