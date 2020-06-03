78d49392ebc5ddfba305d6e61ae82a259226d478
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-06-03 15:36:05 -0700

    notes on palindrome and Big O

--- palindrome/index.js
+++ palindrome/index.js
@@ -9,25 +9,30 @@
 //   palindrome("abcdefg") === false
 
 
-//#1 most compact solution
+//#1 most compact and straight forward solution.
+//Big O(n)
+
 //PSUEDO CODE: Reverse the string then compare the reversed string to the original.
-function palindrome(str) {
- const pal = str.split('').reverse().join('');
- return pal === str;
-}
-
- module.exports = palindrome;
-
-//#2 Not best but is Using advanced array helper : array.every() 
-
+   //Function takes in a string as a parameter to check if its a palindrome 
 // function palindrome(str) {
+    //Variable to hold a copy of the finished product
+    //Take the string, split it into an array, reverse it, join it back to a string
+//  const pal = str.split('').reverse().join('');
+    //Compare given string to the same string but reversed
+//  return pal === str;
+// }
+//  module.exports = palindrome;
+
+//#2 Not best but is Using advanced array helper : array.every()
+//Bit O(n)
+// function palindrome(str) {
+    //Can be returned right away. take given string, split, ".every" method to do something to every character
 //  return str.split('').every((char, i)=>{
+     //Return the boolean of whether or not the two strings match
+     //str.length is the length of the string. 
+     //"- i" is iterating over entire string
+     //and "- 1" because it starts with a zero index
 //   return char === str[str.length - i - 1]
 //  });
-// }
-// module.exports = palindrome;
-
-// function palindrome(str) {
- 
 // }
 // module.exports = palindrome;
