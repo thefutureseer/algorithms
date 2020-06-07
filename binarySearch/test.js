0f719065e2ae9094e163b30f5b142eef9188a554
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-06-07 08:45:34 -0700

    notes for binary search algo

--- binarySearch/test.js
+++ binarySearch/test.js
@@ -4,6 +4,10 @@ test('binarySearch is a function', ()=> {
   expect(typeof binarySearch).toEqual('function');
 });
 
-test('key is 56 return true', () => {
-  expect(binarySearch([1,3,7,8,9,45,56], 56)).toBeTruthy();
+test('key is 56 greater than the mid point & return true', () => {
+  expect(binarySearch([1,3,7,8,9,10,45,56], 56)).toBeTruthy();
+});
+
+test('Key is less than mid point & return true', () => {
+  expect(binarySearch([1,3,7,8,9,10,45,56], 3)).toBeTruthy();
 });
