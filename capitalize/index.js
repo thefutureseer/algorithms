b1e189f6333a12de2543108d4e74ddc82a700e16
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-06-08 20:32:02 -0700

    ran over capitalize and added big O n

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,25 +1,25 @@
 //Capitalize the first letter of every word in a string
 
 
-function capitalize(str) {
-  //Create empty string with first letter capitalized
-  let result = str[0].toUpperCase();
+// function capitalize(str) {
+//   //Create empty string with first letter capitalized
+//   let result = str[0].toUpperCase();
   
-  //Loop through string, check if character to the left is a space, capitalize current character
-  //and add it to result string 
-  for (let i = 1; i < str.length; i++) {
-    if (str[i-1] === ' ') {
-      result += str[i].toUpperCase();
-    } else {
-         //if its not a space simply add current character to the result string .
-         result += str[i];
-      }
-  }
-  //Return new string
-  return result;
-}
+//   //Loop through string, check if character to the left is a space, capitalize current character
+//   //and add it to result string 
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i-1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//          //if its not a space simply add current character to the result string .
+//          result += str[i];
+//       }
+//   }
+//   //Return new string
+//   return result;
+// }
 
-module.exports = capitalize;
+// module.exports = capitalize;
 
 //Create empty array
 //Take a string loop through splitting it by spaces.
@@ -43,3 +43,38 @@ module.exports = capitalize;
 //   //Join words together with a space between each and return the result
 //   return words.join(' ');
 // }
+
+// function capitalize(str) {
+//   //Make an empty array named words
+//   const words = [];
+//   //Split the string by spaces to get an array
+//   //Capitalize first letter of every string
+//   for (let byWord of str.split(' ')) {
+//       words.push(byWord[0].toUpperCase() + byWord.slice(1));
+//   }
+//   //join with spaces
+//   return words.join(' ');
+// }
+
+
+
+//Set first letter of string to capital
+ function capitalize(str) {
+   let capString = str[0].toUpperCase()
+   for (let i = 1; i < str.length; i++) {
+     if (str[i - 1] === ' ') {
+       capString += str[i].toUpperCase();
+     } else {
+        capString += str[i];
+     }
+  }
+   return capString;
+ }
+//const firstCap = str[0].toUpperCase();
+//Loop through to check if theres a space to the left if so capitalize the character we are looking at
+//
+
+//RUNTIME IS linear, BIG O(n)
+
+
+module.exports = capitalize;
