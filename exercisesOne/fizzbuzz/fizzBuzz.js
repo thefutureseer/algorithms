05b2b1c069566b5a421496a99b24d1cfc61be213
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-06-21 10:33:35 -0700

    explain entire algorithm and explain why big O is n

--- exercisesOne/fizzbuzz/fizzBuzz.js
+++ exercisesOne/fizzbuzz/fizzBuzz.js
@@ -1,43 +1,33 @@
 //PSUEDO CODE
-//write a program that prints out numbers 1-100
+//write a program that prints out numbers 1-n (n being the given number)
 //For every 5th number print out fizz instead of the number
 //every third number print buzz instead
-//if both print fizzbuzz!
+//if both 3 & 5 print fizzbuzz!
 
-// function fizzBuzz(n) {
-// //  var n = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-//  for (i=1; i <= n; i++) {
-// //is the number a multiple of 3 or 5?
-//   if (i % 3 ===0 && i % 5 === 0) {
-//       console.log("fizzbuzz");
-//     }
-//     else if ( i % 3 === 0 ) {
-//       console.log("buzz");
-//     }
-//     else if (i % 5 === 0) {
-//       console.log("fizz");
-//     }
-  
-//     else {
-//       console.log(i);
-//     }
-//   }
-// }
-// module.exports = fizzBuzz;
-
+//Function takes in a given number
 function fizzBuzz(n) {
-  for (let i =1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzbuzz')
-    } else if (i % 3 === 0) {
-      console.log('fizz');
-    } else if (i % 5 === 0) {
-      console.log('buzz');
-    } else {
+  //For loop to go through each number from 1 to n
+  for (let i = 1; i <= n; i++) {
+     //Check each number to see if it can be divided by 15 if so print fizzbuzz (all lowercase) to the console
+     if (i % 15 === 0) {
+      console.log("fizzbuzz" );
+    } 
+    //Check each number if it can be divided by 3 if so print fizz (all lowercase) to the console 
+    else if (i % 3 === 0) {
+      console.log("fizz");
+    }
+    //check each number if it can be divided by 5 and if so pirn buzz (lowercase) to the console
+    else if (i % 5 === 0) {
+      console.log("buzz");
+    } 
+    else {
+      //Otherwise print the number 
       console.log(i);
     }
   }
 }
 
-
 module.exports = fizzBuzz;
+
+//TIME COMPLEXITY Big 0(n) it has one loop taking all the time and adding an equal amount of time with 
+//every integer added. All other operations are constant time.
