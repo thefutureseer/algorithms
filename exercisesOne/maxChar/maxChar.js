c01725f7b6e89ee67335f4239c4a8d2e6be54931
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-07-11 15:54:47 -0700

    update with explanation of each line and Big O explanation

--- exercisesOne/maxChar/maxChar.js
+++ exercisesOne/maxChar/maxChar.js
@@ -1,170 +1,71 @@
-//Given a string, return the character most commonly used in the string
-//Example ('345777') === 7 , ('asdfghjjj') === j
-
-// //?make a function to take a string as an arguement?
-// function maxChar(str) {
-// //?set a const for an object
-//   const charMap = {};
-//   let max = 0;
-//   let maxChar = '';
-
-//   for (let char of str) {
-//     if (charMap[char]) {
-//       charMap[char]++;
-//     } else {
-//       charMap[char] = 1;
-//     }
-//   }
-
-//   for (let char in charMap) {
-//     if (charMap[char] > max) {
-//       max = charMap[char];
-//       maxChar = char;
-//     }
-//   }
-
-//   return maxChar;
-// }
-
-// module.exports = maxChar;
+//Time complexity 
+//Big O(n) 
+//Because doubling the number of inputs affects the growth of the algorithm proportionally.
 
 //Given a string, return the character most commonly used in the string
-//Example ('345777') === 7 , ('asdfghjjj') === j
 
 //PSUEDO CODE 
-//Turn string into an array
-//find the matching ascii for each.  OR parse the array for multiple characters
-//push each match to an array
-//join array back to a string
-//return the string
+//LOOP THROUGH STRING finding matchs and keeping count of each
 
-
-//PSUEDO CODE #2
-//LOOP THROUGH STRING to find match and keep count of each
-
-// function maxChar(str) {
-
-// }
-// module.exports = maxChar;
-
-//letters
-// function maxChar(str) {
-//  const charsMap = {};
-//  let max = 0;
-//  let maxChar = "";
-//  for ( let i of str) {
-//    if (charsMap[i]) {
-//      charsMap[i]++;
-//    } else {
-//      charsMap[i] = 1;
-//    }
-//  }
-//    for (let i in charsMap) {
-//      if (charsMap[i] > max) {
-//        max = charsMap[i];
-//        maxChar = i;
-//      }
-//    }
-//    return maxChar;
-// }
-
-// module.exports = maxChar;
-
-//letters one liner
-// function maxChar(str) {
-//   const str = "Hello There";
-//   const chars = {};
-//   for (let i of str) {
-//     chars[i] = chars[i] + 1 || 1;
-//   }
-// }
-
-//PSUEDO CODE to get the most occuring letter/number 
-//First Take a string (then a number) 
-//make an empty object
-//turn the string into an object map
-//return the most common letter/number
+// Function called maxChar to take in given string as an argument
+function maxChar(str) {
+  // Variable of empty object to create a map of each key being character and value being the count of that character 
+  const charsMap = {};
+  // Variable to keep track of the highest value
+  let max = 0;
+  // Variable to keep track of which string character has the highest value
+  let maxChar = "";
 
 //MORE MANUAL LOOP WITH IF STATEMENT:
-// function maxChar(str) {
-//  const str = "Hello There";
-//  const char = {};
-//  for (let i of str) {
-//    if (!char[i]) {
-//      char[i]=1;
-//    } else {
-//      char[i]++;
-//    }
-//  }
-// }
-// module.exports = maxChar;
-
-//PSUEDO CODE to get the most occuring letter/number 
-//First Take a string (then a number) 
-//make an empty object
-//turn the string into an object map
-//loop throught the object
-//return the most common letter/number
-
-// function maxChar(str) {
-// //  const str = "Hello There";
-//  const charMap = {};
-//  let max = 0;
-//  let maxChar = "";
-//  for (let i of str) {
-//    charMap[i] = charMap[i] + 1 || 1;
-//  }
-//  //iterate through the object and return the character most frequently used:
-//  //This is a direct straight forward approach.
-//  for (let i in charMap) {
-//    if (charMap[i] > max) {
-//     max = charMap[i];
-//     maxChar = i;
-//    }
-//  }
-//  return maxChar;
-// //  for (let i in ch) {
-// //   maxChar[i]++;
-// //   letter[i];
-// //  }
-// }
-// module.exports = maxChar;
-
-//PSUEDO CODE to get the most occuring letter/number 
-//First Take a string (then a number) 
-//make an empty object
-//turn the string into an object map
-//return the most common letter/number
-
-// function maxChar(str) {
-
-// }
-// module.exports = maxChar;
-
-//PSUEDO CODE to get the most occuring letter/number 
-//First Take a string (or a number) 
-//and make an empty object
-//turn the string into an object map
-//iterate through the object 
-//count the times a letter comes up
-//update the letter when its most used
-//return the most common letter/number
-
-function maxChar(str) {
- const mapObj = {};
- let maxChar = '';
- let maxNum = 0;
-
- for (let i of str) {
-   mapObj[i] = mapObj[i] + 1 || 1;
-  
- }
- for (let i in mapObj) {
-   if (mapObj[i] > maxNum) {
-     maxNum = mapObj[i];
-     maxChar = i;
+// For loop through the given string to build map: the keys as characters and values as count of that character
+ for ( let i of str) {
+// IF there is a character increment by one  ELSE set key: character value to one
+   if (charsMap[i]) {
+     charsMap[i]++;
+   } else {
+     charsMap[i] = 1;
    }
  }
- return maxChar;
+// For loop through charsMap object to find the higest value 
+   for (let i in charsMap) {
+// IF charsMap at index has a greater value than variable "max", set max to new value and maxChar to new character string
+     if (charsMap[i] > max) {
+       max = charsMap[i];
+       maxChar = i;
+     }
+   }
+// Return maxChar which will be the character with the most reoccurances in the given string
+   return maxChar;
 }
+
 module.exports = maxChar;
+
+
+//Psuedo code
+//Given a string return the most reoccuring character
+//Variable with empty object to count each key which will be each new character
+//for loop through the given string and add to the object keys with each new character and add one for every repeated 
+//character
+//Check for highest reoccuring character
+//Return highest one
+
+
+// function maxChar(str) {
+//  var countmap = {};
+//  var max = 0;
+//  var charMax = '';
+
+//  for (let i of str) {
+//    countmap[i] = countmap[i] + 1 || 1
+//    }
+//    //Loop through countmap for highest number and return
+//    for (let i in countmap) {
+//        if (countmap[i] > max) {
+//          max = countmap[i] ;
+//          charMax = i;
+//        }
+//       }
+//     return charMax;
+//  }
+  
+// module.exports = maxChar;
