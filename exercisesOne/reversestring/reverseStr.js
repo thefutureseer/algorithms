8c9c7e052e1a2229ace51c8607d36b5d56a5337a
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-08-18 16:59:30 -0700

    add big o and why

--- exercisesOne/reversestring/reverseStr.js
+++ exercisesOne/reversestring/reverseStr.js
@@ -1,41 +1,31 @@
-// function reverse(str) {
-//  let jEqualsAccumulativeReversedString = '';
-//  for (let i of str) {
-//  jEqualsAccumulativeReversedString = i + jEqualsAccumulativeReversedString;
-//  }
-//  return jEqualsAccumulativeReversedString;
-// }
-// module.exports = reverse;
+//BIG O time complexity & why:
+//Big O(n)
+//It takes the same amount of time for each peice of data to be
+//handled and the time increases by the same amount per. 
 
 //Given a string, return a new string in the reverse order.
-//REVERSE === ESREVER , STOP === POTS
-//Pseudo code=== edoc oduesP
-//1 Get the index and change the last to the first etc
-//2 Find a great library for strings
-//3 turn string into an ARRAY, Reverse array, array back to string, RETURN string
-// a = [S,T,O,P];
+//E.I. : reverse(REVERSE) === ESREVER , reverse(STOP) === POTS
 
-//#1 psuedo code=> #1
-// 1) turn the string into an array
-// 2) reverse the array
-// 3) take away the commas => turn the array into a string
-// 4) return the string now reversed
 
-function reverse(str) {
- return str.split('').reverse().join('');
-}
-
-module.exports = reverse;
-
-//#1.5
 // function reverse(str) {
-//  const arr = str.split('');
-//  arr.reverse();
-//  return arr.join('');
+//   let accume = '';
+//   for (i of str) {
+//     accume = i + accume;
+//   };
+//   return accume;
 // }
 // module.exports = reverse;
 
-//#2 PSUEDO CODE #2 A LITTLE MORE MANUAL
+
+// psuedo code=> 
+// 1) turn the string into an array
+// 2) reverse the array
+// 3) turn the array into a string
+// 4) return the string now reversed
+
+
+
+// PSUEDO CODE #2 A LITTLE MORE MANUAL
 //Create an empty string variable
 //for of loop through each character of string provided
 //take the character and add it to the empty string variable
@@ -63,38 +53,6 @@ module.exports = reverse;
 // module.exports = reverse;
 // ......--------------------------------------------------------------------
 
-//self reverse a string: string=array=reverse=string: return FAILED TWICE//
-//#1 vanilla javascript?
-// let str = 'abcd';
-// function reverse(str) {
-//  return str.split('').reverse().join('');
-// }
-// reverse(str);
-
-// module.exports = reverse;
-
-// ......--------------------------------------------------------------------
-//#2 without reverse because its too easy. for of loop 2015  FAILED TWICE
-// function reverse(str) {
-//  let reversed = '';
-//  for(let i of str) {
-//   reversed = i + reversed;
-//  }
-//  return reversed;
-
-// }
-// module.exports = reverse;
-
-
-// ......--------------------------------------------------------------------
-// #3 more manual style
-
-// function reverse(str) {
-//  return str.split('').reduce((accumulateString, stringValueNow) => {
-//    return stringValueNow + accumulateString;
-//   }, '');
-// };
-// module.exports = reverse
 
 // ......--------------------------------------------------------------------
 //#1.0 easy  PASSED January 2nd 2020
@@ -104,7 +62,7 @@ module.exports = reverse;
 // module.exports = reverse;
 
 // ......--------------------------------------------------------------------
-//#1.5 2nd easiest PASSED! 1-2-2020
+//#1.5 2nd easiest PASSED 1-2-2020
 // function reverse(str) {
 //   const arr = str.split('');
 //   arr.reverse();
@@ -113,7 +71,7 @@ module.exports = reverse;
 // module.exports = reverse;
 
 // ......--------------------------------------------------------------------
-//#2 without reverse because its too easy. for of loop 2015 PASSED 1-2-2020
+//#2 without reverse method. for of loop 2015 PASSED 1-2-2020
 // function reverse(str) {
 //   let reversed = '';
 //   for (let i of str) {
@@ -145,71 +103,21 @@ module.exports = reverse;
 //  }
 //  module.exports = reverse;
 
+        // ......--------------------------------------------------------------------
+        //#1.0 easy
 
-// ......--------------------------------------------------------------------
-//#2 without reverse because its too easy. Use for of loop 2015
+        // function reverse(str) {
+          //  return str.split('').reverse().join('');
+          // }
+          
+          // module.exports = reverse;
 
-
-// ......--------------------------------------------------------------------
-// #3 more manual style
-
-
-
-// ......--------------------------------------------------------------------
-//#1.0 easy
-
-
-
-// ......--------------------------------------------------------------------
-//#1.5 easiest
-
-
-
-
-
-// ......--------------------------------------------------------------------
-//#2 without reverse because its too easy. for of loop 2015 
-
-
-
-// ......--------------------------------------------------------------------
-// #3 more manual style use reduce instead of a for loop:
+//#2
+//Create a variable and change the variable every step of the way
+//Then return the variable:
 // function reverse(str) {
-//   return str.split('').reduce((j,i) => j = i + j);
+//   let rev = str.split('');
+//   rev.reverse();
+//   return rev.join('');
 // }
 // module.exports = reverse;
-
-// ......--------------------------------------------------------------------
-//#1.0 easy
-// function reverse(str) {
-//   return str.split('').reverse().join('');
-// }
-// module.exports = reverse;
-
-
-// ......--------------------------------------------------------------------
-//#1.5 easiest with a variable and 3 lines.
-// function reverse(str) {
-//   const arr = str.split('');
-//   arr.reverse();
-//   return arr.join('');
-// }
-// module.exports = reverse;
-
-// ......--------------------------------------------------------------------
-//#2 more manual style without reverse because its too easy. (for ...of) loop 2015 
-// function reverse(str) {
-//   let reversed = '';
-//   for (let i of str) {
-//     reversed = i + reversed;
-//   }
-//   return reversed;
-// }
-// module.exports = reverse;
-
-// ......--------------------------------------------------------------------
-// #3 more manual style use reduce instead of a for loop:
-//  function reverse(str) {
-//    return str.split('').reduce((i, j)=> j + i, '')
-//  }
-//  module.exports = reverse;
