cfa70acd1c29bbd2e078ba6e75e5885b1f4a7cd3
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-03-15 18:28:27 -0700

    finish queue and weave

---
+++ exercisesThree/queue.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Implement a 'peek' method in this Queue class.
+// Peek should return the last element (the next
+// one to be returned) from the queue *without*
+// removing it.
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  peek() {
+    return this.data[this.data.length-1]
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+module.exports = Queue;
