766175acdae3e16c3128039fc12d9fe07cc64910
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-04-02 09:41:58 -0700

    add two sum

---
+++ exercisesThree/two-sum/index.js
@@ -0,0 +1,21 @@
+//find two numbers in an array equaling the target number
+//max array length 10,000 or (10 4)
+
+function twoSum(arr, target) {
+  let map = new Map();
+
+  //loop over array to create a map
+  for (let i = 0; i < arr.length; i++) {
+    let pointer1 = arr[i];
+    let pointer2 = target - pointer1;
+
+    if (map.has(pointer2)) {
+      console.log([i, map.get(pointer2)])
+      return [i, map.get(pointer2)];
+    }
+    map.set(pointer1, i);
+    console.log(map);
+  }
+}
+
+twoSum([1,2,3,4,5,6], 11)
