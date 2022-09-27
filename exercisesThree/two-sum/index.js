a5c173769d15f9a96bcf08deab34c070eb55262d
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-09-27 14:33:53 -0700

    going over twosum

--- exercisesThree/two-sum/index.js
+++ exercisesThree/two-sum/index.js
@@ -1,4 +1,9 @@
-//find two numbers in an array equaling the target number
+// what two numbers from the nums array add up to the target param number?
+// return the index of those numbers
+// assume there will be only 2 numbers in the given array
+//NEVER USE THE SAME ELEMENT TWICE
+//EACH WILL HAVE EXACTLY ONE SOLUTION
+
 //max array length 10,000 or (10 4)
 
 function twoSum(arr, target) {
@@ -17,3 +22,5 @@ function twoSum(arr, target) {
 }
 
 module.exports = twoSum;
+
+//using new Map() Time Complexity = O(n) & Space complexity = O(n)
