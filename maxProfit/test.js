35413cefbcffcfe3b3edf9d17a89b0a4fd12fd2c
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-10-03 16:42:27 -0700

    add more tests

--- maxProfit/test.js
+++ maxProfit/test.js
@@ -1,2 +1,20 @@
-const maxProfit = require('./maxProfit');
+//test every part of the algorithm
+const getMaxProfit = require('./maxProfit');
 
+test('maxProfit is a funtion', ()=>{
+ //this:
+ var maxProfits= getMaxProfit;
+ //is this:
+ expect(typeof maxProfits).toEqual('function');
+});
+
+// test('Does maxprofit work', ()=>{
+//   expect(maxProfit([10, 7, 5, 8, 11, 9])).toEqual(
+//     6
+//   )
+// })
+
+// test('should run example code', () => {
+//   // creates a new mocked function with no formal arguments.
+//   expect(example.function.name).toEqual('square');
+//   expect(example.function.length).toEqual(0);
