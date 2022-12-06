605d15600567c2464ee07ec513776e4d92612477
thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-12-05 17:20:21 -0800

    validate bst

--- treeBuild/levelWidth/treeOrderValidation/index.js
+++ treeBuild/levelWidth/treeOrderValidation/index.js
@@ -1,15 +1,20 @@
-function validate(node, min=null, max=null) {
-  if (max !== null && node.data < max  ) {
+function validate(node, min = null, max = null) {
+  if (node.val == min || node.val == max) {
       return false;
   }
-
-  if (min !== null && node.data > min) {
+  if (max !== null && node.val > max) {
       return false;
   }
-
   
-
-
-}
+  if (min !== null && node.val < min) {
+      return false;
+  }
+  
+  if (node.left && !validate(node.left, min, node.val)) { return false; }
+  
+  if (node.right && !validate(node.right, node.val, max)) { return false; }
+ 
+     return true;
+ }
 
 module.exports = validate;
