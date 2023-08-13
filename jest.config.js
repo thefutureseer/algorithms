d38f6dc7886663e69f631f58347c827875d1cb55
thefutureseer <danielharrislimos@outlook.com>
2023-08-12 23:23:50 -0700

    configure jest

---
+++ jest.config.js
@@ -0,0 +1,59 @@
+//TAKING THIS FILE OUT BECAUSE IT WORKS WITHOUT IT
+
+// module.exports = {
+//   watchman: false,
+//   // roots: ['<rootDir>/typeScript/queue/__tests__/'], // Set the root directory for Jest to start looking for tests
+//   testMatch: [
+//     '**/typeScript/queue/__tests__/**/*.ts', // Match test files in __tests__ directories
+//     '**/?(*.)+(spec|test).ts', // Match files with .spec.ts or .test.ts suffix
+//   ],
+//   testPathIgnorePatterns: ['/node_modules/'], // Ignore node_modules directory when searching for tests
+//   // transform: {
+//   //   '^.+\\.ts$': 'ts-jest', // Use ts-jest to transform TypeScript files
+//   // },
+// };
+
+
+
+
+// module.exports = {
+//     watchman: false,
+
+//   roots: ['algorithms/typeScript/queue'], // Set the root directory for Jest to start looking for tests
+//   testMatch: [
+//     '**/__tests__/**/*.ts', // Match test files in __tests__ directories
+//     '**/?(*.)+(spec|test).ts', // Match files with .spec.ts or .test.ts suffix
+//   ],
+//   testPathIgnorePatterns: ['/node_modules/'], // Ignore node_modules directory when searching for tests
+// };
+
+// module.exports = {
+//   watchman: false,
+
+//   roots: ['algorithms'], // Set the root directory for Jest to start looking for tests
+//   testMatch: [
+//     '**/*.spec.ts', // Match files with .spec.ts suffix
+//     '**/*.test.ts', // Match files with .test.ts suffix
+//   ],
+//   testPathIgnorePatterns: ['/node_modules/'], // Ignore node_modules directory when searching for tests
+// };
+
+// module.exports = {
+//   roots: ['<rootDir>/Users/firedragonnow/algorithms/algorithms/typeScript/queue/__tests__/queue.test.ts'], // Define your root directories
+//   testMatch: ['**/typeScript/queue/__test__/queue.test.ts**/*.ts', '**/?(*.)+(spec|test).ts'],
+//   testPathIgnorePatterns: ['/node_modules/'],
+// };
+
+
+
+// module.exports = {
+//   // ... other config options
+
+//   testMatch: [
+//     '**algorithm/algorithm/typeScript/queue/__tests__/test.js**/*.js', // Match test files in the __tests__ directory
+//     '**/?(*.)+(spec|test).js', // Match files with .spec.ts or .test.ts suffix
+//   ],
+//   testPathIgnorePatterns: [
+//     '/node_modules/',
+//   ],
+// };
