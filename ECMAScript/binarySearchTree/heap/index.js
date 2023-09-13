2a81ba89f9244dba30ae548ca6cc7a3e40ff39df
thefutureseer <danielharrislimos@outlook.com>
2023-09-13 14:51:30 -0700

    comments to start

--- ECMAScript/binarySearchTree/heap/index.js
+++ ECMAScript/binarySearchTree/heap/index.js
@@ -1,1 +1,14 @@
-//min heap and max heap. insert, order
+//min heap and max heap.
+//insert and put in order, remove and put in order.
+//equation to find the parent = i / 2 & round down
+//equation to find the left child i * 2
+//equation to find the right child i * 2 + 1
+
+//array first index always set to null
+var heapArr = [null];
+
+const minHeap = (num) => {
+  //push into heap
+
+
+}
