a41eb7e48b38be95f1c4c09e1c878b9dcd4a24c6
thefutureseer <danielharrislimos@outlook.com>
2023-10-10 21:26:28 -0700

    rewrote bubble sort and tests

--- ECMAScript/sort/bubblesort.js
+++ ECMAScript/sort/bubblesort.js
@@ -1,33 +1,62 @@
-//sort an unordered array with bubble sort
-let arrayOne = [1,4,6,5,4,3,1,7,9,5,2]
-//flag to let us know if the sorting is done
-let swapped;
-
-function sort(arr) {
-  //value to decrement for the do while loop
-  let end = arr.length -1;
-  swapped = false;
-
-  for (let i = 0, j =1; i < end; i++, j++) {
-    if (arr[i] > arr[j]) {
-      swapped = true;
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      
-    }
-  }
-  end--;
-};
-
-do {
-  sort(arrayOne);
-} while (swapped);
-
-console.log(arrayOne);
-
-
-
 // //sort an unordered array with bubble sort
 // let arrayOne = [1,4,6,5,4,3,1,7,9,5,2]
+// //flag to let us know if the sorting is done
+// let swapped;
+
+// function sort(arr) {
+//   //value to decrement for the do while loop
+//   let end = arr.length -1;
+//   swapped = false;
+
+//   for (let i = 0, j =1; i < end; i++, j++) {
+//     if (arr[i] > arr[j]) {
+//       swapped = true;
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+      
+//     }
+//   }
+//   end--;
+// };
+
+// do {
+//   sort(arrayOne);
+// } while (swapped);
+
+// console.log(arrayOne);
+
+
+function bubbleSort(arr) {
+    var n = arr.length;
+    var swapped;
+  
+    do {
+        swapped = false;
+    
+        for (var i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // Swap the elements
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+              }
+            }
+          } while (swapped);
+        
+          return arr;
+        }
+        module.exports = bubbleSort;
+
+
+        
+        // // Example usage:
+        // var unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+        // var sortedArray = bubbleSort(unsortedArray);
+        // console.log(sortedArray);
+        
+        
+        // //sort an unordered array with bubble sort
+        // let arrayOne = [1,4,6,5,4,3,1,7,9,5,2]
 // //flag to let us know if the sorting is done
 // let swapped;
 
