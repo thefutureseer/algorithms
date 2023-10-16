e918295bdbf55a2794548ea228455ebc12022094
thefutureseer <danielharrislimos@outlook.com>
2023-10-16 13:55:06 -0700

    what array fits

---
+++ ECMAScript/chunk/whatArrayFits/index.js
@@ -0,0 +1,15 @@
+//Given an array of numbers
+//How many numbers in the array can we return
+//Before going over the given maximum target number
+
+//psuedo code 
+//#1 can take all the first numbers until one pushes over but that
+//might not squeeze as many as possible in to the returned array
+
+//#2 can order the numbers in the array from smallest to largest
+// and take all the smallest first that way we have the most numbers
+
+//#3 we can pick and choose what we want in the returned array until
+//it reaches the maximum allowed.
+
+//Note: we can write tests first with jest
