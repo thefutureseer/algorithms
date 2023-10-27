9592e67906af108bdd5d474a00587f8a9914218e
thefutureseer <danielharrislimos@outlook.com>
2023-10-26 19:11:53 -0700

    add test

--- ECMAScript/sort/selectionSort/test.js
+++ ECMAScript/sort/selectionSort/test.js
@@ -11,3 +11,10 @@ test("order numbers in array [1, 3, 5, 7, 2]", ()=>{
   console.log("func ",arr3)
   expect(arr3).toEqual(arr2);
 });
+
+test("if array only has one, [1]", () => {
+  const arr = [1];
+  const arr2 = [1];
+  const arr3 = sort(arr);
+  expect(arr3).toEqual(arr2);
+})
