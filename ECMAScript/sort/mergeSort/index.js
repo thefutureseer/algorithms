6a7363674dd1c210d100410615eb7063592f2125
thefutureseer <danielharrislimos@outlook.com>
2023-10-29 21:46:58 -0700

    add left and right pointer

--- ECMAScript/sort/mergeSort/index.js
+++ ECMAScript/sort/mergeSort/index.js
@@ -1,34 +1,116 @@
+function merge(leftArray, rightArray) {
+  const resultsArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      if (leftArray[leftIndex] < rightArray[rightIndex]) {
+          resultsArray.push(leftArray[leftIndex]);
+          leftIndex++;
+      } else {
+          resultsArray.push(rightArray[rightIndex]);
+          rightIndex++;
+      }
+  }
+
+  // After the while loop, one of the arrays might still have elements
+  while (leftIndex < leftArray.length) {
+      resultsArray.push(leftArray[leftIndex]);
+      leftIndex++;
+  }
+
+  while (rightIndex < rightArray.length) {
+      resultsArray.push(rightArray[rightIndex]);
+      rightIndex++;
+  }
+
+  return resultsArray;
+}
+
+
+
+// function merge(leftArray, rightArray) {
+//   const resultsArray = [];
+//   let i = 0;
+
+//   while (i < leftArray.length && i < rightArray.length) {
+//       if (leftArray[i] < rightArray[i]) {
+//           resultsArray.push(leftArray[i]);
+//       } else {
+//           resultsArray.push(rightArray[i]);
+//       }
+//       i++;
+//   }
+
+//   while (i < leftArray.length) {
+//       resultsArray.push(leftArray[i]);
+//       i++;
+//   }
+
+//   while (i < rightArray.length) {
+//       resultsArray.push(rightArray[i]);
+//       i++;
+//   }
+
+//   return resultsArray;
+// }
+
+
+//merge psuedocode
+//take 2 sorted arrays
+// const merged = (arr1, arr2) => {
+//   //array to store the results
+//   let resultsArr = [];
+//   //while both arrays have a number in them
+//   while(arr1[i] | arr2[i]) {
+//     resultsArr.push(arr1[i])
+//   }
+
+  //while the left array is the only array
+
+  //while the right array is the only array
+
+//   return resultsArr;
+
+// };
+
+
 
 //Merge funciton psuedocode
 //take two sorted arrays and merge into one
-function merge(arr1, arr2) {
-  //create results array
-  let resultsArr = [];
+// function merge(arr1, arr2) {
+//   //create results array
+//   let resultsArr = [];
+
+//   //while there are still elements in both arrays
+//   var i = 0;
+//   while (arr1 && arr2) {
+//     //& if the first element in the left array is less than the first in the right
+//     //shift the element from the left into the results array
+//     //else shift the element from the right array into the results array
+//     if(arr1[i] < arr2[i]) {
+//       resultsArr.push(arr1[i]);
+//     } resultsArr.push(arr2);
+//     i++;
+//   }
   
-  //while there are still elements in both arrays
-  var i = 0;
-  while (arr1 && arr2) {
-    //& if the first element in the left array is less than the first in the right
-    //shift the element from the left into the results array
-    //else shift the element from the right array into the results array
-    if(arr1[i] < arr2[i]) {
-      resultsArr.push(arr1[i]);
-    } resultsArr.push(arr2);
-    i++;
-  }
-  
-  while (i < arr1.length) {
-    resultsArr.push(arr1[i]);
-    i++
-  };
+//   //take everything from the array 
+//   //that still has stuff in it and push to results
+//   while (i < arr1.length) {
+//     resultsArr.push(arr1[i]);
+//     i++
+//   };
 
-  while (i < arr2.length) {
-    resultsArr.push(arr2[i]);
-    i++
-  }
+//   while (i < arr2.length) {
+//     resultsArr.push(arr2[i]);
+//     i++
+//   }
 
-  return resultsArr;
+//   return resultsArr;
 
-}
+// }
 
-//take everything from the array that still has stuff in it and push to results
+
+//  console.log(merge([1,2,45], [3,4,6,8]));
+
+module.exports = merge;
