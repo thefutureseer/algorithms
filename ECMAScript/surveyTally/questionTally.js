66bf76f934839ae6668fbf6e27894706e5a9543d
thefutureseer <danielharrislimos@outlook.com>
2024-06-29 03:08:04 -0700

    add tests and try catch

--- ECMAScript/surveyTally/questionTally.js
+++ ECMAScript/surveyTally/questionTally.js
@@ -1,4 +1,5 @@
-const inputJsonObj = '[{"_id":"66585a86f96fa5443587abdc","respondentName":"Field supervisor S.O.C.","teamPerformance":"Over all the team did good.  We caught one person doing the wrong thing and that person was sent home.","supportTeam":"The Warriors","vampireHighSchool":false,"forgetMemory":"The nightmares.","favoriteToy":"Memory (the card game)","comfortFood":"Tacos","snackPreference":"Savory","dreamDestination":"Spain but mostly unknown as for a dream destination maybe the moon.","memorableTravel":"I almost got married in Guayaquil, Ecuador.","reincarnationAnimal":"Lion or an Eagle ","datingPreference":"All the time","Love":true},{"_id":"665db0766b972f7c791b8d43","respondentName":"F.S. S.O.C.","teamPerformance":"Pretty good. 1 person missing. ","teamSupporterOf":"Warriors / Bay area teams","vampireHighSchool":false,"forgetAMemory":"Nightmares","favoriteToyOrGame":"Memory the card game.","comfortFood":"Chocolate","snackPreference":"Savory","dreamDestination":"As for a dream idk but maybe the moon!","memorableTravel":"I almost got married in Guayaquil, Ecuador","campingAnyone?":"850","petPreference":"other","petPreference-Comment":"animal person","reincarnationAnimal":"A Lion or an Eagle.","religion":true,"DatingPreferenceRe:Persistence":"All the time","mystics?":"500"}]';
+// const inputJsonObj = '[{"_id":"66585a86f96fa5443587abdc","respondentName":"Field supervisor S.O.C.","teamPerformance":"Over all the team did good.  We caught one person doing the wrong thing and that person was sent home.","supportTeam":"The Warriors","vampireHighSchool":false,"forgetMemory":"The nightmares.","favoriteToy":"Memory (the card game)","comfortFood":"Tacos","snackPreference":"Savory","dreamDestination":"Spain but mostly unknown as for a dream destination maybe the moon.","memorableTravel":"I almost got married in Guayaquil, Ecuador.","reincarnationAnimal":"Lion or an Eagle ","datingPreference":"All the time","Love":true},{"_id":"665db0766b972f7c791b8d43","respondentName":"F.S. S.O.C.","teamPerformance":"Pretty good. 1 person missing. ","teamSupporterOf":"Warriors / Bay area teams","vampireHighSchool":false,"forgetAMemory":"Nightmares","favoriteToyOrGame":"Memory the card game.","comfortFood":"Chocolate","snackPreference":"Savory","dreamDestination":"As for a dream idk but maybe the moon!","memorableTravel":"I almost got married in Guayaquil, Ecuador","campingAnyone?":"850","petPreference":"other","petPreference-Comment":"animal person","reincarnationAnimal":"A Lion or an Eagle.","religion":true,"DatingPreferenceRe:Persistence":"All the time","mystics?":"500"}]';
+// '[{"_id":"1","vampireHighSchool":true},{"_id":"2","vampireHighSchool":false},{"_id":"3","vampireHighSchool":true}]';
 
 const countKeys = (inputJson) => {
   // Initialize the _id counter
@@ -7,9 +8,20 @@ const countKeys = (inputJson) => {
   let highSchoolForever = 0;
   let highSchoolForeverNo = 0;
   let majorityVote;
-
-  // Parse JSON string to an array of objects
-  let arrFromJson = JSON.parse(inputJson);
+  // let vampireHighSchool = Boolean;
+  // Attempt to parse JSON string to an array of objects
+  let arrFromJson;
+  try {
+    arrFromJson = JSON.parse(inputJson);
+  } catch (error) {
+    console.error("Invalid JSON string:", error);
+    return {
+      idCount,
+      highSchoolForever,
+      highSchoolForeverNo,
+      majorityVote: 'invalidJson'
+    };
+  };
 
   // Iterate through each object in the array
   for (let obj of arrFromJson) {
@@ -19,15 +31,15 @@ const countKeys = (inputJson) => {
     if (obj.hasOwnProperty('vampireHighSchool')) {
       if(obj.vampireHighSchool===true //is set to true:
       ) highSchoolForever++;
-        highSchoolForeverNo++;
-      //check which is higher
+      else  highSchoolForeverNo++;
     }
   }
+  //check which is higher
   majorityVote = highSchoolForever > highSchoolForeverNo ? 'highSchoolForever' : 'highSchoolForeverNo';
-
   return {idCount, highSchoolForever, highSchoolForeverNo, majorityVote};
 };
 
-countKeys(inputJsonObj);
+// const keys = countKeys(inputJsonObj);
+// console.log("Heres the data returned: ", keys);
 
 module.exports = countKeys;
--- ECMAScript/surveyTally/test.js
+++ ECMAScript/surveyTally/test.js
@@ -1,13 +1,37 @@
 const countKeys = require("./questionTally");
 
-test("Check if countKeys is a function", ()=>{
-  const countingKeys = countKeys;
-  expect(typeof countingKeys).toEqual('function');
+test("Check if countKeys is a function", () => {
+  expect(typeof countKeys).toEqual('function');
 });
 
-test("Funciton to count id's of takers", ()=>{
-  const inputJsonObj = '[{"_id":"66585a86f96fa5443587abdc","respondentName":"Field supervisor S.O.C."}, {"_id":"66585a86f96fa5443587ab","respondentName":"supervisor S.O.C."},{ "_id":"585a86f96fa5443587abdc","respondentName":" S.O.C."}]'
+test("Function to count id's of takers", () => {
+  const inputJsonObj = '[{"_id":"66585a86f96fa5443587abdc","respondentName":"Field supervisor S.O.C."},{"_id":"66585a86f96fa5443587ab","respondentName":"supervisor S.O.C."},{"_id":"585a86f96fa5443587abdc","respondentName":" S.O.C."}]';
   const numberOfKeys = countKeys(inputJsonObj);
-  console.log(numberOfKeys.idCount, " <--THIS IS NUM KEY")
+  console.log(numberOfKeys, " THIS IS NUM KEY");
   expect(numberOfKeys.idCount).toEqual(3);
+});
+
+test("Function handles empty input", () => {
+  const inputJsonObj = '[]';
+  const numberOfKeys = countKeys(inputJsonObj);
+  expect(numberOfKeys.idCount).toEqual(0);
+});
+
+test("Function handles invalid JSON", () => {
+  const inputJsonObj = 'invalid-json';
+  const numberOfKeys = countKeys(inputJsonObj);
+  expect(numberOfKeys.majorityVote).toEqual('invalidJson');
+});
+
+test("Function counts correctly without _id key", () => {
+  const inputJsonObj = '[{"respondentName":"Field supervisor S.O.C."},{"respondentName":"supervisor S.O.C."}]';
+  const numberOfKeys = countKeys(inputJsonObj);
+  expect(numberOfKeys.idCount).toEqual(0);
+});
+
+test("Function counts correctly with mixed true/false for vampireHighSchool", () => {
+  const inputJsonObj = '[{"_id":"1","vampireHighSchool":true},{"_id":"2","vampireHighSchool":false},{"_id":"3","vampireHighSchool":true}]';
+  const numberOfYays = countKeys(inputJsonObj);
+  console.log("num of yeses: json obj: ", numberOfYays.highSchoolForever)
+  expect(numberOfYays.highSchoolForever).toEqual(2);
 });
