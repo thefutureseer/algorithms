094eef0b49ba2cebf8318a1fe201241e3df9924d
thefutureseer <danielharrislimos@outlook.com>
2024-07-01 21:33:32 -0700

    finish mergesort

--- ECMAScript/sort/mergeSort/test.js
+++ ECMAScript/sort/mergeSort/test.js
@@ -13,3 +13,43 @@ test("take 2 arrays and merge them", () => {
   const arr3 = [1, 2, 3, 4, 5, 7, 8, 9];
   expect(mergeOne).toEqual(arr3);
 });
+
+test("one item in array", ()=>{
+  const arrone = [ 1 ];
+  const arrtwo = [];
+  const mergeOne = merge(arrone, arrtwo);
+  const arr3 = [1]
+  expect(mergeOne).toEqual(arr3);
+});
+
+test("both arrays are empty", () => {
+  const arrone = [];
+  const arrtwo = [];
+  const mergeOne = merge(arrone, arrtwo);
+  const arr3 = [];
+  expect(mergeOne).toEqual(arr3);
+});
+
+test("each array has one element", () => {
+  const arrone = [3];
+  const arrtwo = [1];
+  const mergeOne = merge(arrone, arrtwo);
+  const arr3 = [1, 3];
+  expect(mergeOne).toEqual(arr3);
+});
+
+test("arrays with duplicate elements", () => {
+  const arrone = [1, 3, 5, 5];
+  const arrtwo = [2, 4, 5, 6];
+  const mergeOne = merge(arrone, arrtwo);
+  const arr3 = [1, 2, 3, 4, 5, 5, 5, 6];
+  expect(mergeOne).toEqual(arr3);
+});
+
+test("arrays with negative numbers", () => {
+  const arrone = [-3, -1, 2, 4];
+  const arrtwo = [-2, 0, 1, 3];
+  const mergeOne = merge(arrone, arrtwo);
+  const arr3 = [-3, -2, -1, 0, 1, 2, 3, 4];
+  expect(mergeOne).toEqual(arr3);
+});
