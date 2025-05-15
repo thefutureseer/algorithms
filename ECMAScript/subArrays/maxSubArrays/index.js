9ab18f7460d67c18cff599bf81fcae8f2bd46ee0
thefutureseer <danielharrislimos@outlook.com>
2025-05-14 21:37:55 -0700

    add max sub array q and psuedo

---
+++ ECMAScript/subArrays/maxSubArrays/index.js
@@ -0,0 +1,16 @@
+
+// 2. Function to look for consecutive numbers
+function findConsecNum(takeArray) {
+  // 1. Varables for MaxSubArray, CurrentSubArray
+ 
+  // 3. If the next number is 
+  //  consecutive then add it to the 
+  //  previous number and set it to CurrentSubArray.
+  // 4. If the number is not consecutive 
+  //  to the previous number stop counting 
+  // 5. check if number of CurrentSubArray 
+  // is larger than MaxSubArray if yes then
+  // set MaxSubArray as CurrentSubArray
+  // 6. after looking through the array
+  // return number of MaxSubArray
+};
