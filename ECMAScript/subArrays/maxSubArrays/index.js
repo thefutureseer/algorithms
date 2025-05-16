f001b6bc43adafa4837aa155b8f4d6e11f9f1a06
thefutureseer <danielharrislimos@outlook.com>
2025-05-15 21:41:39 -0700

    fix: max contiguous sum add code

--- ECMAScript/subArrays/maxSubArrays/index.js
+++ ECMAScript/subArrays/maxSubArrays/index.js
@@ -1,16 +1,19 @@
 
-// 2. Function to look for consecutive numbers
+// 2. Function to add contiguous numbers
 function findConsecNum(takeArray) {
-  // 1. Varables for MaxSubArray, CurrentSubArray
- 
-  // 3. If the next number is 
-  //  consecutive then add it to the 
-  //  previous number and set it to CurrentSubArray.
-  // 4. If the number is not consecutive 
-  //  to the previous number stop counting 
-  // 5. check if number of CurrentSubArray 
-  // is larger than MaxSubArray if yes then
-  // set MaxSubArray as CurrentSubArray
-  // 6. after looking through the array
-  // return number of MaxSubArray
+  // 1. Varables for MaxSubArray, Currentmax
+  const maxSubArray = takeArray[0];
+  const Currentmax = takeArray[0];
+  for(var i = 1; i < takeArray.length; i++){ 
+    // 3. If the next number is higher
+    //  than previous number set 
+    // it to max.
+    Currentmax = Math.max(Currentmax, takeArray[i]);
+    maxSubArray = Math.max(maxSubArray, Currentmax);
+    }
+    // 4. If the number is not higher 
+    //  then reset to current number in array 
+    // 5. after looking through the array
+    // return number of MaxSubArray
+    return maxSubArray;
 };
